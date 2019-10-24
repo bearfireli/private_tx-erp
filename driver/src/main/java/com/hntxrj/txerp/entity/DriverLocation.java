bef2786aadfacf6c1f3429a450c5b3d4b02fac00
haoranliu <2388399752@qq.com>
2019-10-24 09:02:28 +0800

    司机位置

---
+++ driver/src/main/java/com/hntxrj/txerp/entity/DriverLocation.java
@@ -0,0 +1,28 @@
+package com.hntxrj.txerp.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+/**
+ * 记录司机位置
+ */
+@Data
+public class DriverLocation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer dlId;
+    private String dlEppCode;
+    private Integer dlTsiId;
+    private String dlDriverCode;
+    private String dlCarId;
+    private String address;
+    private Timestamp createTime;
+
+
+}
---
+++ driver/src/main/java/com/hntxrj/txerp/mapper/DriverLocationMapper.java
@@ -0,0 +1,7 @@
+package com.hntxrj.txerp.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DriverLocationMapper {
+}
