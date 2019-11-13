937a46a7660eba93f1af5173905f3fe2cc19a8ce
chenshiqiang <shiqiang_chen@126.com>
2019-11-13 11:47:32 +0800

    autoAddAuth

---
+++ erp-driver/src/test/java/com/hntxrj/txerp/txerp/DriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hntxrj.txerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
