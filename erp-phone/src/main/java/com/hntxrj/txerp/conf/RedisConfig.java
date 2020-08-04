a0c7e90cb92ccb7e34025f768632c9777158bfe5
NewSkyy <1102298375@qq.com>
2020-08-04 18:00:47 +0800

    删除driverRedisTemplate

--- erp-phone/src/main/java/com/hntxrj/txerp/conf/RedisConfig.java
+++ erp-phone/src/main/java/com/hntxrj/txerp/conf/RedisConfig.java
@@ -55,31 +55,6 @@ public class RedisConfig extends CachingConfigurerSupport {
         return template;
     }
 
-
-    @Bean
-    public RedisTemplate<String, List<VehicleIdVO>> driverPushRedisTemplate(RedisConnectionFactory factory) {
-
-        RedisTemplate<String, List<VehicleIdVO>> template = new RedisTemplate<>();
-
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-
-        template.setConnectionFactory(factory);
-        //key序列化方式
-        template.setKeySerializer(redisSerializer);
-        //value序列化
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        //value hashmap序列化
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-
-        return template;
-    }
-
     /**
      * json序列化
      * org.springframework.data.redis.serializer.RedisSerializer<T>
